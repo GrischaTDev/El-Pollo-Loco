979ad92e56e88b6e9a16e48a6f92d3a6928573e7
Grischa <Grischa.Taenzer@gmail.com>
2024-04-04 21:07:22 +0200

    Add first classes

---
+++ js/game.js
@@ -0,0 +1,10 @@
+let canvas;
+let ctx;
+let charakter = new MovableObject();
+
+function init() {
+    canvas = document.getElementById('canvas');
+    ctx = canvas.getContext('2d');
+
+    console.log('My Charakter is', charakter);
+}
---
+++ models/movable-object.class.js
@@ -0,0 +1,9 @@
+class MovableObject {
+    x = 400;
+    y = 150;
+    img;
+
+    moveRight() {
+        console.log('Moving Right');
+    }
+}
